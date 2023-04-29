5f7a7c7d0b32448f86f0c1a595722f8292251e79
mukunthans <smukunthan134@gmail.com>
2023-04-29 21:35:57 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <DataProvider>
           <NavBar />
           <SearchBar />
